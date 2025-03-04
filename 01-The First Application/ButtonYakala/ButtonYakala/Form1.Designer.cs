ffb87cbf796139418e05a79570f5f3993947a401
Erhan AKAGÜNDÜZ <bes.erhan@gmail.com>
2025-03-04 21:42:24 +0300

    The First Application
    The First Application

---
+++ 01-The First Application/ButtonYakala/ButtonYakala/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ButtonYakala
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 130);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Beni Yakala";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(92, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 360);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Butonu Yakala";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 01-The First Application/ButtonYakala/ButtonYakala/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ButtonYakala
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Buton yakalandığında ekrana mesaj veririz
+            MessageBox.Show("Tebrikler Butonu Yakaladın");
+            //Formun arka plan rengini değiştiriyoruz
+            this.BackColor = Color.MediumVioletRed;
+            //label1 in başlangıçta boş olan text ine yazı yazıyoruz
+            label1.Text = "Afferin Bak ne yaptın";
+            //label1 in yazı rengini beyaz yapıyoruz
+            label1.ForeColor = Color.White;
+            //butonumuzu saklıyoruz
+            button1.Hide();
+
+        }
+
+        // Rastgele sayı üretmek için Random nesnesi
+        Random r = new Random();
+        // butonun boyutunu takip eden boyut isimli değişken
+        int boyut = 50;
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            // butonun boyutunu 5 piksel artır
+            boyut += 5;
+
+            // butonun pencere içindeki dikey (Y) konumunu rastgele değiştir
+            // (0 ile pencerenin yüksekliği - 50 arasında bir değer seç)
+            button1.Top = r.Next(0, this.Height - 50);
+
+            // butonun pencere içindeki yatay (X) konumunu rastgele değiştir
+            // (0 ile pencerenin genişliği - 50 arasında bir değer seç)
+            button1.Left = r.Next(0, this.Width - 50);
+
+            // butonun yeni boyutunu ayarla (fareyle üzerine her gelindiğinde buton büyür)
+            button1.Width = boyut;
+            button1.Height = boyut;
+        }
+
+    }
+}
